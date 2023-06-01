038c4ae7132e3649cadf77d21d781a94421a109e
Duchnikovsky <filipduchnik@outlook.com>
2023-06-01 15:58:32 +0200

    server check2

--- back-end/server.ts
+++ back-end/server.ts
@@ -11,11 +11,11 @@ const cors = require('cors')
 const app = express()
 app.use(express.json());
 
-app.use(cors({
-  origin: [process.env.CLIENT_URL],
-  methods: ["GET", "POST"],
-  credentials: true,
-}))
+// app.use(cors({
+//   origin: [process.env.CLIENT_URL],
+//   methods: ["GET", "POST"],
+//   credentials: true,
+// }))
 
 // app.use(cookieParser())
 // app.use(bodyParser.urlencoded({extended: true}))
